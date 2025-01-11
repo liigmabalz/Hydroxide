67a42db7d9662a36b24f4f33fa935fd82a1c5f94
liigmabalz <148204291+liigmabalz@users.noreply.github.com>
2025-01-11 16:00:51 -0300

    Update RemoteSpy.lua

--- ui/modules/RemoteSpy.lua
+++ ui/modules/RemoteSpy.lua
@@ -18,7 +18,7 @@ local MessageBox, MessageType = import("ui/controls/MessageBox")
 local ContextMenu, ContextMenuButton = import("ui/controls/ContextMenu")
 local TabSelector = import("ui/controls/TabSelector")
 
-local Base = import("rbxassetid://11389137937").Base
+local Base = import("rbxassetid://81417807894535").Base -- 11389137937
 local Assets = import("rbxassetid://5042114982").RemoteSpy
 
 local Prompts = Base.Prompts
