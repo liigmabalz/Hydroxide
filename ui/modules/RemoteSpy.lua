82c2edfb62d42325cc2083c1013e45f201d2bf67
liigmabalz <148204291+liigmabalz@users.noreply.github.com>
2025-01-11 17:36:20 -0300

    Update RemoteSpy.lua

--- ui/modules/RemoteSpy.lua
+++ ui/modules/RemoteSpy.lua
@@ -61,11 +61,10 @@ local icons = {
     ignore = "rbxassetid://4842578510",
     unignore = "rbxassetid://4842578818",
     RemoteEvent = "rbxassetid://120968494043889", -- 4229806545
-    RemoteFunction = "rbxassetid://76491890194535", -- 4229810474
-    BindableEvent = "rbxassetid://70425784049266", -- 4229809371
-    BindableFunction = "rbxassetid://70425784049266"
+    RemoteFunction = "rbxassetid://123389199715910", -- 4229810474
+    BindableEvent = "rbxassetid://85225432986297", -- 4229809371
+    BindableFunction = "rbxassetid://109302544925686"
 }
-
 local constants = {
     fadeLength = TweenInfo.new(0.15),
     textWidth = Vector2.new(1337420, 20),
