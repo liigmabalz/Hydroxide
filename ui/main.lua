7f0b37ecdb50f9492626ea774a2ffa1905d35ebf
liigmabalz <148204291+liigmabalz@users.noreply.github.com>
2025-01-11 15:54:55 -0300

    Update main.lua

--- ui/main.lua
+++ ui/main.lua
@@ -2,7 +2,7 @@ local CoreGui = game:GetService("CoreGui")
 local UserInput = game:GetService("UserInputService")
 local HttpService = game:GetService("HttpService")
 
-local Interface = import("rbxassetid://122525006768292")  -- 11389137937
+local Interface = import("rbxassetid://11389137937")  -- 11389137937
 
 if oh.Cache["ui/main"] then
 	return Interface
